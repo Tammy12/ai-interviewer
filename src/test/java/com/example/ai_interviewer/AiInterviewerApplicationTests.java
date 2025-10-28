917b50e889b76cb266b113fe3059eb673d300643
Tammy Xu <tammyxu21@gmail.com>
2025-10-28 13:35:48 -0500

    initializing repo

---
+++ src/test/java/com/example/ai_interviewer/AiInterviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_interviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
